b923c03de744a52adc39b02a05657a752acbf4ee
kwabsntim <daffourkwabena@gmail.com>
2025-10-29 21:41:46 +0000

    gitignore

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -11,17 +11,16 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/integration/mtest"
 )
 
-// creating mocking structs for testing
-// ----------fake MongoDB collection------------
-
 func TestCreateUser(t *testing.T) {
 	// Setup mock MongoDB
 	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
 	mt.Run("success", func(mtT *mtest.T) {
 		// Mock responses: user doesn't exist, then insert succeeds
 		mtT.AddMockResponses(
+			//if the mocktest does not return a user insert instead ie 0
 			mtest.CreateCursorResponse(0, "usersdb.users", mtest.FirstBatch), // FindOne returns nothing
-			mtest.CreateSuccessResponse(),                                    // InsertOne succeeds
+			//user created successfully
+			mtest.CreateSuccessResponse(), // InsertOne succeeds
 		)
 		payload := `{"username":"fake user","email":"fake@example.com","password":"fake1234"}`
 		//creating the request for creating a user
@@ -49,4 +48,38 @@ func TestCreateUser(t *testing.T) {
 			t.Errorf("expected message %s but got %s", expected, response.Message)
 		}
 	})
+}
+func TestCreateUser_InvalidEmail(t *testing.T) {
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	mt.Run("invalid email", func(mtT *mtest.T) {
+		payload := `{"email":"invalidemail"}`
+		req := httptest.NewRequest(http.MethodPost, "/api/create-user", bytes.NewBufferString(payload))
+		rec := httptest.NewRecorder()
+		h := &Handler{
+			Client: mtT.Client,
+		}
+		h.CreateUser(rec, req)
+		if rec.Code != http.StatusBadRequest {
+			t.Fatal("Expected 400 but got", rec.Code)
+		}
+	})
+}
+func TestCreateUser_UserExits(t *testing.T) {
+	mt := mtest.New(t, mtest.NewOptions().ClientType(mtest.Mock))
+	mt.Run("User exists", func(mtT *mtest.T) {
+		mtT.AddMockResponses(
+			mtest.CreateCursorResponse(1, "usersdb.users", mtest.FirstBatch),bson.M{"email":"xxx@mail.com"}),
+		)
+		payload := `{"username":"FakeUser","email":"fakeemail@mail.com"}`
+		req := httptest.NewRequest(http.MethodPost, "/api/create-user", bytes.NewBufferString(payload))
+		rec := httptest.NewRecorder()
+		h := &Handler{
+			Client: mtT.Client,
+		}
+		h.CreateUser(rec, req)
+		if rec.Code != http.StatusConflict {
+			t.Fatalf("Expected 409 but got %d", rec.Code)
+		}
+	})
+
 }
